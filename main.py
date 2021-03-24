968e80a79e426fb40edcf9f76659119c8a5cf60f
AlDS90 <lkjs0JH)0fw3vcSFDSF9>
2021-03-24 14:53:22 +0700

    add pushButton_2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
 import psycopg2 as pg
 import datetime
+import pandas as pd
+import os
 from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QIcon
 from ui import Ui_MainWindow
@@ -28,6 +30,8 @@ def get_s_kns(li_kns: list) -> str:
 class App(QtWidgets.QMainWindow):
     def __init__(self):
         super(App, self).__init__()
+        self.date, self.db_name_main, self.db_name_first, self.db_name_second = '', '', '', ''
+        self.li_data_main_db, self.li_data_first_db, self.li_data_second_db = [], [], []
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.d_tb = {'Земли сельзоз назначения': (self.ui.table_sx, 0),
@@ -41,6 +45,7 @@ class App(QtWidgets.QMainWindow):
         self.setFixedSize(self.size())
         [self.init_tb(key) for key in self.d_tb.keys()]
         self.ui.pushButton.clicked.connect(self.start_analyze)
+        self.ui.pushButton_2.clicked.connect(self.unload_to_excel)
 
     def init_tb(self, key: str):
         tb = self.d_tb[key][0]
@@ -64,28 +69,44 @@ class App(QtWidgets.QMainWindow):
 
     def start_analyze(self):
         [self.clear_tb(key) for key in self.d_tb.keys()]
-        date = self.ui.dateEdit.text()
-        db_name_main = self.ui.comboBox.currentText()
-        self.ui.tabWidget.setCurrentIndex(self.d_tb[db_name_main][1])
-        db_name_first, db_name_second = [key for key in self.d_tb.keys() if key != db_name_main]
-        li_kns_main_db = db.get_data(date, db_name_main, db.q_get_kns)
+        [data.clear() for data in [self.li_data_main_db, self.li_data_first_db, self.li_data_second_db]]
+        self.date = self.ui.dateEdit.text()
+        self.db_name_main = self.ui.comboBox.currentText()
+        self.ui.tabWidget.setCurrentIndex(self.d_tb[self.db_name_main][1])
+        self.db_name_first, self.db_name_second = [key for key in self.d_tb.keys() if key != self.db_name_main]
+        li_kns_main_db = db.get_data(self.date, self.db_name_main, db.q_get_kns)
         if li_kns_main_db:
-            li_data_first_db = del_double(db.get_data(date, db_name_first, db.q_get_data_first_and_second_dbs,
-                                                      get_s_kns(li_kns_main_db)))
-            li_data_second_db = del_double(db.get_data(date, db_name_second, db.q_get_data_first_and_second_dbs,
-                                                       get_s_kns(li_kns_main_db)))
+            self.li_data_first_db = del_double(db.get_data(self.date, self.db_name_first,
+                                                           db.q_get_data_first_and_second_dbs,
+                                                           get_s_kns(li_kns_main_db)))
+            self.li_data_second_db = del_double(db.get_data(self.date, self.db_name_second,
+                                                            db.q_get_data_first_and_second_dbs,
+                                                            get_s_kns(li_kns_main_db)))
             li_kns_union: list = []
-            li_kns_union.extend([(item[0],) for item in li_data_first_db])
-            li_kns_union.extend([(item[0],) for item in li_data_second_db])
+            li_kns_union.extend([(item[0],) for item in self.li_data_first_db])
+            li_kns_union.extend([(item[0],) for item in self.li_data_second_db])
             if li_kns_union:
-                li_data_main_db = db.get_data(date, db_name_main, db.q_get_data_main_db,
-                                              get_s_kns(li_kns_union))
-                for data, db_name in [(li_data_main_db, db_name_main),
-                                      (li_data_first_db, db_name_first),
-                                      (li_data_second_db, db_name_second)]:
+                self.li_data_main_db = db.get_data(self.date, self.db_name_main, db.q_get_data_main_db,
+                                                   get_s_kns(li_kns_union))
+                for data, db_name in [(self.li_data_main_db, self.db_name_main),
+                                      (self.li_data_first_db, self.db_name_first),
+                                      (self.li_data_second_db, self.db_name_second)]:
                     if data:
                         self.display_records(data, db_name)
         [self.add_one_row(key) for key in self.d_tb.keys()]
+
+    def unload_to_excel(self):
+        main_dir = r'\\192.168.1.40\All\Сотниченко А.Д\programs\is_category_changed'
+        file_name = '{}_{}_{}_analyze_doc.xlsx'.format(datetime.datetime.now().strftime('%d%m%Y_%H%M%S%f_'),
+                                                       self.date,
+                                                       self.db_name_main)
+        li_name_col = tuple([(self.ui.table_sx.horizontalHeaderItem(j).text(), j)
+                             for j in range(self.ui.table_sx.columnCount())])
+        d_data = {name: [row[col] for row in self.li_data_main_db] for name, col in li_name_col}
+        df = pd.DataFrame(d_data)
+        df.to_excel(main_dir + os.sep + file_name,
+                    sheet_name=self.db_name_main,
+                    index=False)
 
     def display_records(self, li_data, db_name):
         tb = self.d_tb[db_name][0]
