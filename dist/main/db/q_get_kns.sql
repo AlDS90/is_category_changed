753ef2b65319a9ea3dfd306acef71d2fd051084b
AlDS90 <lkjs0JH)0fw3vcSFDSF9>
2021-03-30 10:18:59 +0700

    add 3 columns

--- dist/main/db/q_get_kns.sql
+++
@@ -1,5 +0,0 @@
-SELECT cadastralnum
-FROM bufferbase JOIN listforrating
-                ON listforrating_unid = listforrating.unid AND
-                   listforrating.valuationdate = %(date)s
-ORDER BY cadastralnum
